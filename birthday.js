83599e7d03adbf4b8e9e64f0e4f40492adf392c1
KevinPham <kevinpham15@gmail.com>
2017-09-25 15:36:07 -0700

    made small change to code

--- birthday.js
+++ birthday.js
@@ -4,15 +4,15 @@ if(daysUntilMyBirthday > 30)
 {
     console.log("such a looong time till my birthday... =[");
 }
-else if(daysUntilMyBirthday <= 30)
+if(daysUntilMyBirthday <= 30)
 {
     console.log("almost my birthday!");
 }
-else if(daysUntilMyBirthday > 5)
+if(daysUntilMyBirthday < 5)
 {
-    console.log("JUST 5 MORE DAYS")
+    console.log("JUST 5 MORE DAYS");
 }
 if(daysUntilMyBirthday = 0)
 {
-    console.log("IT'S MY BIRTHDAY!!")
+    console.log("IT'S MY BIRTHDAY!!");
 }
