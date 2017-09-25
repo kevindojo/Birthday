bfdd9fa1f6e99a93d7a9ed942c4e755cc4637e9a
KevinPham <kevinpham15@gmail.com>
2017-09-25 15:30:31 -0700

    birthday complete

---
+++ birthday.js
@@ -0,0 +1,18 @@
+var daysUntilMyBirthday = 70;
+
+if(daysUntilMyBirthday > 30)
+{
+    console.log("such a looong time till my birthday... =[");
+}
+else if(daysUntilMyBirthday <= 30)
+{
+    console.log("almost my birthday!");
+}
+else if(daysUntilMyBirthday > 5)
+{
+    console.log("JUST 5 MORE DAYS")
+}
+if(daysUntilMyBirthday = 0)
+{
+    console.log("IT'S MY BIRTHDAY!!")
+}
